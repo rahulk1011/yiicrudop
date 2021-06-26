c54d4ed1ad02d7bfb6e7344f4106684af2545115
Rahul Khan <khanrahul.1011@gmail.com>
2021-06-27 02:28:34 +0530

    First Commit

---
+++ models/SignupForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\models;
+use yii\base\Model;
+use yii\helpers\VarDumper;
+
+class SignupForm extends Model
+{
+    public $username;
+    public $password;
+    public $confirm_password;
+
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'confirm_password'], 'required'],
+            [['username', 'password', 'confirm_password'], 'string',  'min' => 4, 'max' => 16],
+            ['confirm_password', 'compare', 'compareAttribute' => 'password'],
+        ];
+    }
+
+    public function signup()
+    {
+        $user = new User();
+        $user->username = $this->username;
+        $user->password = \Yii::$app->security->generatePasswordHash($this->password);
+        $user->access_token = \Yii::$app->security->generateRandomString();
+        $user->auth_key = \Yii::$app->security->generateRandomString();
+
+        if($user->save()) {
+            return true;
+        }
+        \Yii::error('User was not Saved. '.VarDumper::dumpAsString($user->errors));
+        return false;
+    }
+}
